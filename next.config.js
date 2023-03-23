a6e04183d835a20b5b0d3bc72d69cab14fbdf2a8
Mina-1316 <mina1316.canreply@gmail.com>
2023-03-23 22:05:55 +0900

    restart project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: '/'
-  }
 }
+
+module.exports = nextConfig
--- pages/testpage/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function test() {
-  return (
-    <div className="font-bold underline">
-      <p>안녕하세요</p>
-    </div>
-  )
-}
