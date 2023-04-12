5778edc38e34987ddb4f5e93144cdeb212dac7d9
Mina-1316 <mina1316.canreply@gmail.com>
2023-04-12 22:16:03 +0900

    create font template - use tailwind format 'font-[]' to apply

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import GlobalStyles from '@/components/boundary/GlobalStyles/GlobalStyles'
 import '@/styles/globals.css'
+import '@/styles/fonts.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
